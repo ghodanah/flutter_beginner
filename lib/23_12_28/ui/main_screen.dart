7ef6075d8ee5fe2ba4304a513373ce7fc7f2e7b2
ghoda_nah <hhh1529@gmail.com>
2023-12-28 17:30:51 +0900

    UI 뼈대 만들기

--- lib/23_12_28/ui/main_screen.dart
+++ lib/23_12_28/ui/main_screen.dart
@@ -5,6 +5,53 @@ class MainScreen2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Color(0xFF4FB6B2),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Color(0xFF4FB6B2),
+                    ),
+                  ),
+                  hintText: 'Search',
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.search,
+                      color: Color(0xFF4FB6B2),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                  ),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Image.network('https://cdn.pixabay.com/photo/2017/09/26/13/21/apples-2788599_150.jpg');
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
